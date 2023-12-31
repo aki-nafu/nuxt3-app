1836b378e394fde3aa2c9d3b217501b21a8a877b
aki-nafu <akimifunakoshi@gmail.com>
2023-12-31 17:44:42 +0900

    天気情報を取得

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>天気予報</h1>
+    <div v-if="weatherData">
+      <!-- 天気情報の表示 -->
+      <p>{{ weatherData.title }}</p>
+      <p v-for="forecast in weatherData.forecasts" :key="forecast.dateLabel">
+        {{ forecast.dateLabel }}: {{ forecast.telop }}
+      </p>
+    </div>
   </div>
 </template>
+
+<script setup>
+const { data: weatherData, pending, error } = await useFetch(
+  'https://weather.tsukumijima.net/api/forecast/city/130010'
+)
+</script>
