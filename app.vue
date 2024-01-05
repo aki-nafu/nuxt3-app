27fe84631f3bbd9a418452a7a92846bcf3fddcb1
aki-nafu <akimifunakoshi@gmail.com>
2024-01-05 19:37:22 +0900

    動的に天気マークが表示されるようになった

--- app.vue
+++ app.vue
@@ -4,7 +4,8 @@
     <div v-if="weatherData">
       <div v-for="(day, index) in weatherData.daily.time" :key="index">
         日付: {{ day.toLocaleDateString() }}
-        <br>天気: {{ getWeatherName(weatherData.daily.weatherCode[index]) }}
+        <br>天気: <img :src="getWeatherImage(weatherData.daily.weatherCode[index])" alt="Weather icon">
+        {{ getWeatherImage(weatherData.daily.weatherCode[index]) }}
         <br>最高気温: {{ weatherData.daily.temperature2mMax[index] }}°C
         <br>最低気温: {{ weatherData.daily.temperature2mMin[index] }}°C
         <br>降水確率: {{ weatherData.daily.precipitationProbabilityMax[index] }}%
@@ -14,11 +15,14 @@
     <div v-else>
       天気データの取得中...
     </div>
+    <img :src="`assets/icons/${sunny}.png`" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+
+const sunny = "sunny.png"
 
 const weatherData = ref(null);
 
@@ -52,39 +56,40 @@ const fetchData = async () => {
   }
 };
 
-const getWeatherName = (code: string) => {
-  const weatherCodeToName = {
-    '0': '晴れ',
-    '1': '晴れ時々曇り',
-    '2': '晴れ一時雨',
-    '3': '晴れ時々雪',
-    '45': '霧',
-    '48': '霧凍',
-    '51': '霧雨',
-    '53': '霧雨',
-    '55': '霧雨',
-    '56': '霧凍雨',
-    '57': '霧凍雨',
-    '61': '一時雨',
-    '63': '一時雨',
-    '65': '一時雨',
-    '66': '一時凍雨',
-    '67': '一時凍雨',
-    '71': '一時雪',
-    '73': '一時雪',
-    '75': '一時雪',
-    '77': 'みぞれ',
-    '80': 'にわか雨',
-    '81': 'にわか雨',
-    '82': 'にわか雨',
-    '85': 'にわか雪',
-    '86': 'にわか雪',
-    '95': '雷雨',
-    '96': '雷雨と雹',
-    '99': '雷雨と雹'
+const getWeatherImage = (code: string) => {
+  const weatherCodeToImageUrl = {
+    '0': 'assets/icons/sunny.png',
+    '1': 'assets/icons/sunny_cloud.png',
+    '2': 'assets/icons/sunny_rain.png',
+    '3': 'assets/icons/sunny_snow.png',
+    '45': 'assets/icons/rain_cloud.png',
+    '48': 'assets/icons/rain_cloud.png',
+    '51': 'assets/icons/rain_cloud.png',
+    '53': 'assets/icons/rain_cloud.png',
+    '55': '.assets/icons/rain_cloud.png',
+    '56': '.assets/icons/rain_cloud.png',
+    '57': 'assets/icons/rain_cloud.png',
+    '61': 'assets/icons/cloud_rain.png',
+    '63': 'assets/icons/cloud_rain.png',
+    '65': 'assets/icons/cloud_rain.png',
+    '66': 'assets/icons/cloud_rain.png',
+    '67': 'assets/icons/cloud_rain.png',
+    '71': 'assets/icons/cloud_snow.png',
+    '73': 'assets/icons/cloud_snow.png',
+    '75': 'assets/icons/cloud_snow.png',
+    '77': 'assets/icons/cloud_snow.png',
+    '80': 'assets/icons/rain.png',
+    '81': 'assets/icons/rain.png',
+    '82': 'assets/icons/rain.png',
+    '85': 'assets/icons/snow_cloud.png',
+    '86': 'assets/icons/snow_cloud.png',
+    '95': 'assets/icons/rain.png',
+    '96': 'assets/icons/rain_snow.png',
+    '99': 'assets/icons/rain_snow.png'
   };
 
-  return weatherCodeToName[code] || '不明';
+  return new URL(`${weatherCodeToImageUrl[code]}`, import.meta.url).href ;
+  
 };
 
 onMounted(fetchData);
